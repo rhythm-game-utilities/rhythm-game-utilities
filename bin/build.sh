2cc69c574cb69598f3e3106dfbfd988fdd9c8d79
Scott Doxey <neogeek0101@gmail.com>
2026-08-11 01:00:43 -0400

    Removed logic for generating multiple arch builds and then combining them.

--- bin/build.sh
+++ bin/build.sh
@@ -15,16 +15,9 @@ COLOROFF=$(tput sgr0)
 
     printf "Building macOS libraries ... "
 
-    g++ -std=c++17 -shared -fPIC -arch arm64 -o build/libRhythmGameUtilities-arm64.dylib include/RhythmGameUtilities/RhythmGameUtilities.cpp
-    g++ -std=c++17 -shared -fPIC -arch x86_64 -o build/libRhythmGameUtilities-x86_64.dylib include/RhythmGameUtilities/RhythmGameUtilities.cpp
-
-    if [[ ! -f build/libRhythmGameUtilities-arm64.dylib || ! -f build/libRhythmGameUtilities-x86_64.dylib ]]; then
-        exit 1
-    fi
-
-    lipo -create -output build/libRhythmGameUtilities.dylib build/libRhythmGameUtilities-arm64.dylib build/libRhythmGameUtilities-x86_64.dylib
-
-    lipo -info build/libRhythmGameUtilities.dylib
+    g++ -std=c++17 -shared -fPIC -arch arm64 -arch x86_64 \
+        -o build/libRhythmGameUtilities.dylib \
+        include/RhythmGameUtilities/RhythmGameUtilities.cpp || exit 1
 
     mkdir -p UnityPackage/Libs/macOS
     mkdir -p RhythmGameUtilities/Libs/macOS
