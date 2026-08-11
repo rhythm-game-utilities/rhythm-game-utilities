f5ea48d93176723958e8440ccb70e61f70bf4d2b
Scott Doxey <neogeek0101@gmail.com>
2026-08-11 00:53:57 -0400

    Whitespace.

--- bin/generate_docs.sh
+++ bin/generate_docs.sh
@@ -4,58 +4,58 @@ SCRIPT_DIR=$(cd -- "$(dirname -- "${BASH_SOURCE[0]}")" &>/dev/null && pwd)
 
 (
 
-  mkdir -p build/
+    mkdir -p build/
 
-  cd "${SCRIPT_DIR}" || exit
+    cd "${SCRIPT_DIR}" || exit
 
-  cd ..
+    cd ..
 
-  {
-    (cat "./Documentation/README.md" && printf '\n')
-    (cat "./Documentation/Platforms.md" && printf '\n')
-    (cat "./Documentation/Examples.md" && printf '\n')
-    (cat "./Documentation/Install.md" && printf '\n')
-  } > README.md
+    {
+        (cat "./Documentation/README.md" && printf '\n')
+        (cat "./Documentation/Platforms.md" && printf '\n')
+        (cat "./Documentation/Examples.md" && printf '\n')
+        (cat "./Documentation/Install.md" && printf '\n')
+    } > README.md
 
-  printf "## API\n\n" >>README.md
+    printf "## API\n\n" >>README.md
 
-  (cat "./Documentation/API/Audio/README.md" && printf '\n') >>README.md
+    (cat "./Documentation/API/Audio/README.md" && printf '\n') >>README.md
 
-  find ./Documentation/API/Audio -type f -name "*.md" -not -name "README.md" | sort -n |
-    while read -r FILE; do
-      (cat "${FILE}" && printf '\n') >>README.md
-    done
+    find ./Documentation/API/Audio -type f -name "*.md" -not -name "README.md" | sort -n |
+        while read -r FILE; do
+            (cat "${FILE}" && printf '\n') >>README.md
+        done
 
-  (cat "./Documentation/API/Common/README.md" && printf '\n') >>README.md
+    (cat "./Documentation/API/Common/README.md" && printf '\n') >>README.md
 
-  find ./Documentation/API/Common -type f -name "*.md" -not -name "README.md" | sort -n |
-    while read -r FILE; do
-      (cat "${FILE}" && printf '\n') >>README.md
-    done
+    find ./Documentation/API/Common -type f -name "*.md" -not -name "README.md" | sort -n |
+        while read -r FILE; do
+            (cat "${FILE}" && printf '\n') >>README.md
+        done
 
-  (cat "./Documentation/API/Parsers/README.md" && printf '\n') >>README.md
+    (cat "./Documentation/API/Parsers/README.md" && printf '\n') >>README.md
 
-  find ./Documentation/API/Parsers -type f -name "*.md" -not -name "README.md" | sort -n |
-    while read -r FILE; do
-      (cat "${FILE}" && printf '\n') >>README.md
-    done
+    find ./Documentation/API/Parsers -type f -name "*.md" -not -name "README.md" | sort -n |
+        while read -r FILE; do
+            (cat "${FILE}" && printf '\n') >>README.md
+        done
 
-  (cat "./Documentation/API/Utilities/README.md" && printf '\n') >>README.md
+    (cat "./Documentation/API/Utilities/README.md" && printf '\n') >>README.md
 
-  find ./Documentation/API/Utilities -type f -name "*.md" -not -name "README.md" | sort -n |
-    while read -r FILE; do
-      (cat "${FILE}" && printf '\n') >>README.md
-    done
+    find ./Documentation/API/Utilities -type f -name "*.md" -not -name "README.md" | sort -n |
+        while read -r FILE; do
+            (cat "${FILE}" && printf '\n') >>README.md
+        done
 
-  {
-    (cat "./Documentation/Architecture.md" && printf '\n')
-    (cat "./Documentation/Git Hooks.md" && printf '\n')
-    (cat "./Documentation/Testing.md" && printf '\n')
-    (cat "./Documentation/Build.md" && printf '\n')
-    (cat "./Documentation/Contributing.md" && printf '\n')
-    (cat "./Documentation/Community Roadmap.md" && printf '\n')
-    (cat "./Documentation/Other Projects.md" && printf '\n')
-    (cat "./Documentation/License.md" && printf '\n')
-  } >> README.md
+    {
+        (cat "./Documentation/Architecture.md" && printf '\n')
+        (cat "./Documentation/Git Hooks.md" && printf '\n')
+        (cat "./Documentation/Testing.md" && printf '\n')
+        (cat "./Documentation/Build.md" && printf '\n')
+        (cat "./Documentation/Contributing.md" && printf '\n')
+        (cat "./Documentation/Community Roadmap.md" && printf '\n')
+        (cat "./Documentation/Other Projects.md" && printf '\n')
+        (cat "./Documentation/License.md" && printf '\n')
+    } >> README.md
 
 )
