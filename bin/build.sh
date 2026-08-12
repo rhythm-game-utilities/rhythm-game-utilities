a314eaef4500f3a9632108cfcfc1a3c6cd68916a
Scott Doxey <neogeek0101@gmail.com>
2026-08-11 23:40:06 -0400

    Simplified the build process.
    Added test to make sure build process works.

--- bin/build.sh
+++ bin/build.sh
@@ -2,6 +2,7 @@
 
 SCRIPT_DIR=$(cd -- "$(dirname -- "${BASH_SOURCE[0]}")" &>/dev/null && pwd)
 
+REDON=$(tput setaf 1)
 GREENON=$(tput setaf 2)
 COLOROFF=$(tput sgr0)
 
@@ -18,6 +19,12 @@ COLOROFF=$(tput sgr0)
     g++ -std=c++17 -shared -fPIC -arch arm64 -arch x86_64 \
         -o build/libRhythmGameUtilities.dylib \
         include/RhythmGameUtilities/RhythmGameUtilities.cpp || exit 1
+
+    if ! lipo -info build/libRhythmGameUtilities.dylib | grep -q "x86_64 arm64"; then
+        printf "%sFAILED%s\n" "${REDON}" "${COLOROFF}"
+
+        exit 1
+    fi
 
     mkdir -p UnityPackage/Libs/macOS
     mkdir -p RhythmGameUtilities/Libs/macOS
