1a8407d1bc4e05039d14165c4a96739883e4786b
Scott Doxey <neogeek0101@gmail.com>
2026-09-23 00:21:09 -0400

    Prevent looking for .meta files in . xcframework folders.

--- bin/test-unity-plugin-meta-files.sh
+++ bin/test-unity-plugin-meta-files.sh
@@ -1,6 +1,9 @@
 #!/bin/bash
 
 find ./UnityPackage -type f -name "*" ! -name "*.meta" ! -name ".DS_Store" | while read -r FILE; do
+    if [[ "${FILE}" == *".xcframework/"* ]]; then
+        continue
+    fi
     if ! [ -f "${FILE}.meta" ]; then
         echo "${FILE}.meta does not exist."
         exit 1
